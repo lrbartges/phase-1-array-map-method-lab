38cbe4449a9ee17ce67761260c2f64ca8f91318d
Lindsey Bartges <lrbartges@gmail.com>
2023-10-04 17:21:02 -0700

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(' ');
+  });
 }
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
